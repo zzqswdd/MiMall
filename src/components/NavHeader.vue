5e29ad11e1d8a10819eb814377c83985cb6c0069
zzqswdd <1216513863@qq.com>
2021-11-03 22:13:14 +0800

    完成目录结构

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "NavHeader",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/order.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from "@/components/NavHeader.vue";
+import NavFooter from "@/components/NavFooter.vue";
+export default {
+  name: "Order",
+  components: {
+    NavHeader,
+    NavFooter,
+  },
+};
+</script>
+
+<style scoped>
+</style>
